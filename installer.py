285291f8e3d929d9c46af17f6c520789a06913ff
plasticmacaroni <jeffblightning@gmail.com>
2022-10-23 22:53:56 -0500

    Use Stages to sort logic

--- installer.py
+++ installer.py
@@ -25,6 +25,8 @@ class Downloader:
         self.disk_check_completed = False
         # File index counter
         self.current_file_index = 0
+        # Install step counter
+        self.set_step = 1
         # Build window
         self.main_message = "Press OK to get started"
         self.step = "Step 1 of 6"
@@ -41,10 +43,6 @@ class Downloader:
 
     def set_message(self, message):
         self.window.Element("MAIN_MESSAGE").update(message)
-        self.window.read()
-
-    def set_step(self, step):
-        self.window.Element("STEP").update("Step " + str(step) + " of 6")
         self.window.read()
 
     def check_size(self, url, local_file):
@@ -83,7 +81,7 @@ class Downloader:
     def run(self):
         # Create an event loop
         while True:
-            # Get window context, this must always come before all other logic
+            # Get window context
             event, values = self.window.read(timeout = 100)
             # First thing, end program if user closes window or presses the OK button
             if event == sg.WIN_CLOSED or event == "CANCEL":
@@ -91,25 +89,10 @@ class Downloader:
             if self.exit_now:
                 self.window.close()
 
-            # Check if running from game folder and set flag
-            if self.folder_checked_flag == False:
-                if not os.path.exists("XwingAlliance.exe"):
-                    self.set_message("It's not recommended to run outside of the game folder, are you sure?\nIf you haven't downloaded X-Wing Alliance from GOG, Steam, or Origin, please do so and run this program from within the game folder.")
-                    self.needs_OK_to_continue = True
-                self.folder_checked_flag = True
-
-            # Check if enough disk space and set flag
-            if not self.disk_check_completed:
-                self.set_message("Checking available space...")
-                available_disk = shutil.disk_usage(pathlib.Path.home().drive).free
-                available_disk_in_gb = available_disk / 1073741824
-                if available_disk_in_gb < 50:
-                    self.set_message("Recommended 50 GB minimum space to install, are you sure you want to continue?")
-                    self.needs_OK_to_continue = False
-                self.disk_check_completed = True
-
-            # Make sure it's been run once
-            # TODO
+            # Set step, which is arbitrary
+            self.step = "Step " + str(self.set_step) + " of 6"
+            self.window.Element("STEP").update(self.step)
+            self.window.read()
 
             # Don't act unless user says yes
             if self.needs_OK_to_continue:
@@ -117,69 +100,101 @@ class Downloader:
                     self.set_message("Continuing...")
                     self.needs_OK_to_continue = False
 
-            # Download current file (current_file_index variable)
-            # If we're requesting an index out of range, great, we've already requested it all
-            if not self.current_file_index >= len(websites):
-                if not os.path.isfile(websites[self.current_file_index].file_name):
-                    self.set_message("Downloading " + websites[self.current_file_index].description + ": " + websites[self.current_file_index].file_name + "\nDownloading thousands of new models, scripts, and events.\n" + "This may take quite a bit, so leave this window open and grab a cup of tea...")
-                    try:
-                        self.download_file(websites[self.current_file_index].url, websites[self.current_file_index].file_name)
-                    except Exception as exc:
-                        print(exc)
-                        self.set_message('Encountered unknown error: '+ str(exc) + '. Press anything to exit.')
-                        self.exit_now = True
-                        continue
-                # Check the filesize to make sure the files are fully downloaded (large files, so partial downloads are a pain to troubleshoot)
-                if websites[self.current_file_index].size_checked == False:
-                    self.set_message("Checking file size for downloaded "+ websites[self.current_file_index].file_name +", press NEXT to continue...")
-                    self.check_size(websites[self.current_file_index].url, websites[self.current_file_index].file_name)
-                    websites[self.current_file_index].size_checked = True
-                    self.set_message("Filesize match. Press NEXT to continue...")
-                # Attempt unzip if the file exists
-                if ".zip" in websites[self.current_file_index].file_name:
-                    try:
-                        print("Unzipping" + " " + websites[self.current_file_index].file_name)
-                        self.set_message("Unzipping" + " " + websites[self.current_file_index].file_name + "...")
-                        zipfile.ZipFile(websites[self.current_file_index].file_name).extractall()
-                    except RuntimeError:
-                        continue
-                self.current_file_index += 1
-                self.set_step(self.current_file_index+1)
-                continue
-            
-            #The continue above means the following code is unreachable before the websites[] are fully visited/downloaded
+            # Step 1: Check if running from game folder and set flag
+            if self.set_step == 1:
+                if self.folder_checked_flag == False:
+                    if not os.path.exists("XwingAlliance.exe"):
+                        self.set_message("It's not recommended to run outside of the game folder, are you sure?\nIf you haven't downloaded X-Wing Alliance from GOG, Steam, or Origin, please do so and run this program from within the game folder.")
+                        self.needs_OK_to_continue = True
+                    self.folder_checked_flag = True
+                self.set_step += 1
 
-            # Install XWAU2020 (checks for install through folder "/TotalConverter"):
-            if not os.path.exists("TotalConverter"):
-                self.set_message("Please complete the XWAU2020 executable and press NEXT when installation is complete.")
-                os.startfile("XWAU2020.exe")
-                self.current_file_index += 1
-                self.set_step(self.current_file_index+1)
-                self.needs_OK_to_continue = True
-                continue
+            # Step 2: Check if enough disk space and set flag
+            if self.set_step == 2:
+                if not self.disk_check_completed:
+                    self.set_message("Checking available space...")
+                    available_disk = shutil.disk_usage(pathlib.Path.home().drive).free
+                    available_disk_in_gb = available_disk / 1073741824
+                    if available_disk_in_gb < 50:
+                        self.set_message("Recommended 50 GB minimum space to install, are you sure you want to continue?")
+                        self.needs_OK_to_continue = False
+                    self.disk_check_completed = True
+
+                    # Make sure it's been run once
+                    # TODO
+                self.set_step += 1
+
+            # Step 3: Download necessary files (current_file_index variable)
+            if self.set_step == 3:
+                # If we're requesting an index out of range, great, we've already requested it all
+                if not self.current_file_index >= len(websites):
+                    if not os.path.isfile(websites[self.current_file_index].file_name):
+                        self.set_message("Downloading " + websites[self.current_file_index].description + ": " + websites[self.current_file_index].file_name + "\nDownloading thousands of new models, scripts, and events.\n" + "This may take quite a bit, so leave this window open and grab a cup of tea...")
+                        try:
+                            self.download_file(websites[self.current_file_index].url, websites[self.current_file_index].file_name)
+                        except Exception as exc:
+                            print(exc)
+                            self.set_message('Encountered unknown error: '+ str(exc) + '. Press anything to exit.')
+                            self.exit_now = True
+                            continue
+                    # Check the filesize to make sure the files are fully downloaded (large files, so partial downloads are a pain to troubleshoot)
+                    if websites[self.current_file_index].size_checked == False:
+                        self.set_message("Checking file size for downloaded "+ websites[self.current_file_index].file_name +", press NEXT to continue...")
+                        self.check_size(websites[self.current_file_index].url, websites[self.current_file_index].file_name)
+                        websites[self.current_file_index].size_checked = True
+                        self.set_message("Filesize match. Press NEXT to continue...")
+                    # Attempt unzip if the file exists
+                    if ".zip" in websites[self.current_file_index].file_name:
+                        try:
+                            print("Unzipping" + " " + websites[self.current_file_index].file_name)
+                            self.set_message("Unzipping" + " " + websites[self.current_file_index].file_name + "...")
+                            zipfile.ZipFile(websites[self.current_file_index].file_name).extractall()
+                        except RuntimeError:
+                            continue
+                    self.current_file_index += 1
+                    continue
+                self.set_step += 1
+            #The continue above means the following code is unreachable before the websites[] are fully visited/downloaded
+            
+            # Step 3: Install XWAU2020 (checks for install through folder "/TotalConverter"):
+            if self.set_step == 3:
+                if not os.path.exists("TotalConverter"):
+                    self.set_message("Please complete the XWAU2020 executable install process and press NEXT when installation is complete.")
+                    os.startfile("XWAU2020.exe")
+                    self.current_file_index += 1
+                    self.needs_OK_to_continue = True
+                    continue
+                self.set_step += 1
 
             # Install TFTC 1.3:
-            if not os.path.exists("TFTCversion.txt"):
-                self.set_message("Please complete the TFTC 1.3 executable and press NEXT when installation is complete.")
-                os.startfile("TFTC_1.3.exe")
-                self.current_file_index += 1
-                self.set_step(self.current_file_index+1)
-                self.needs_OK_to_continue = True
-                continue
+            if self.set_step == 4:            
+                if not os.path.exists("TFTCversion.txt"):
+                    self.set_message("Please complete the TFTC 1.3 executable install process and press NEXT when installation is complete.")
+                    os.startfile("TFTC_1.3.exe")
+                    self.current_file_index += 1
+                    self.needs_OK_to_continue = True
+                    continue
+                self.set_step += 1
 
             # Install TFTC 1.3.2 Patch:
-            if not os.path.exists("TFTC_1.3.2.exe"):
-                self.set_message("Please complete the TFTC 1.3.2 Patch executable and press OK when installation is complete.")
-                os.startfile("TFTC_1.3.2.exe")
-                self.current_file_index += 1
-                self.set_step(self.current_file_index+1)
-                self.needs_OK_to_continue = True
-                continue
+            if self.set_step == 5:    
+                if os.path.exists("TFTCversion.txt"):
+                    with open(r'TFTCversion.txt', 'r') as file:
+                        content = file.read()
+                        if not '1.3.2' in content:
+                            print('string exist')
+                            self.set_message("Please complete the TFTC 1.3.2 Patch executable install process and press OK when installation is complete.")
+                            os.startfile("TFTC_1.3.2.exe")
+                            self.current_file_index += 1
+                            self.needs_OK_to_continue = True
+                            continue
+                self.set_step += 1
 
-            self.set_message("Choose Palpatine Total Converter from the Game Launcher window to start.\nChoose Load a Total Conversion, and then:\n1. TFTC Classic for the original story, remastered\n2. TFTC Reimagined for 8+ new campaigns.\nEnjoy!")
-            os.startfile("TFTC_1.3.2.exe")
-            self.set_step(6)
-            self.exit_now
+            # Step 6: Launch
+            if self.set_step == 6:   
+                self.set_message("Choose Palpatine Total Converter from the Game Launcher window to start.\nChoose Load a Total Conversion, and then:\n1. TFTC Classic for the original story, remastered\n2. TFTC Reimagined for 8+ new campaigns.\nEnjoy!")
+                os.startfile("TFTC_1.3.2.exe")
+                self.exit_now
 
         self.window.close()
 
